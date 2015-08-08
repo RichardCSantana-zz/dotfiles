c8280ac138a0a66514758d3866b7fb92d4d51b72
RichardCSantana <richardcsantana@gmail.com>
2015-08-08 14:32:47 -0300

    adição de atalhos pro docker e vagrant

--- configuraMaquina.sh
+++ configuraMaquina.sh
@@ -2,5 +2,9 @@
 if [ ! -d ~/backup ]; then
   mkdir ~/backup
 fi
+
 mv ~/.gitconfig ~/backup/.gitconfig
 ln -s "${PWD}/.gitconfig" ~/.gitconfig
+
+mv ~/.bashrc ~/backup/.bashrc
+ln -s "${PWD}/.bashrc" ~/.bashrc
